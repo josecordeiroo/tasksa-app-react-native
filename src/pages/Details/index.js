e1b44dd9d282d2741098755a628ade09686a493f
Zeca Cordeiro <zecaxcr@gmail.com>
2022-10-01 20:30:18 -0300

    create screen to updating task

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,11 +1,50 @@
 import React, { useState } from "react";
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 
-export default function Details () {
+import { databaseApp } from "../../config/firebaseconfig";
+import styles from "./style";
+
+import {
+  collection,
+  query,
+  orderBy,
+  addDoc,
+  serverTimestamp,
+  doc,
+  setDoc,
+  deleteDoc,
+  onSnapshot,
+} from "firebase/firestore";
+
+export default function Details ({navigation, route}) {
+  const db = query(collection(databaseApp, "Tasks"));
+
+  const [descriptionEdit, setDescriptionEdit] = useState(route.params.description)
+  const idTask = route.params.id
+
+  const editTask = async () => {
+    await setDoc(doc(db, idTask), {
+      text: descriptionEdit,
+      updatedAt: serverTimestamp(),
+    });
+    navigation.navigate("Task")
+  };
 
   return (
-    <View>
-      <Text>Details Page</Text>
+    <View style={styles.container}>
+      <Text style={styles.label}>Atualizar a tarefa:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Digite a nova tarefa"
+        value={descriptionEdit}
+        onChangeText={setDescriptionEdit}
+      />
+      <TouchableOpacity
+        style={styles.buttonNewTask}
+        onPress={() => editTask()}
+      >
+        <Text style={styles.iconButton}>Salvar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
--- src/pages/Details/style.js
+++ src/pages/Details/style.js
@@ -1,9 +1,43 @@
-import { StyleSheet } from "react-native-web";
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    Details: {
-
-    }
+    container: {
+        flex: 1,
+        backgroudColor: "#fff"
+    },
+    label: {
+        width: "90%",
+        marginTop: 20,
+        marginLeft: 20,
+        fontSize: 16,
+        color: "#f92e6a"
+    },
+    input: {
+        width: "90%",
+        marginTop: 10,
+        padding: 10,
+        height: 50,
+        borderBottomWidth: 1,
+        borderBottomColor: "#f92e6a",
+        marginLeft: "auto",
+        marginRight: "auto"
+    },
+    buttonNewTask: {
+        width: 60,
+        height: 60,
+        position: "absolute",
+        top: 130,
+        right: 20,
+        backgroundColor: "#F92e6a",
+        borderRadius: 50,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    iconButton: {
+        color: "#ffffff",
+        fontSize: 15,
+        fontWeight: "bold",
+    },
 })
 
 export default styles
